fe48c3d66c6d8d9cfea5a331a59f6cee0fb0cbc0
Raphaël Boisvert <raphboisvert14@gmail.com>
2024-03-16 10:14:29 -0400

    hello world

--- jam_cobayes_2024/scenes/Player.gd
+++ jam_cobayes_2024/scenes/Player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var tile_map = $"../TileMap"
+@onready var tile_map = $"../World/TileMap"
 
 var astar_grid: AStarGrid2D
 var current_id_path: Array[Vector2i]
@@ -8,17 +8,21 @@ var target_position: Vector2
 var is_moving: bool
 
 func _ready():
+	await get_tree().process_frame
 	astar_grid = AStarGrid2D.new()
 	astar_grid.region = tile_map.get_used_rect()
-	astar_grid.cell_size = Vector2(64, 32) # 64 by 32 pixels
-	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	# astar_grid.cell_size = Vector2(64, 32) # 64 by 32 pixels
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
+	astar_grid.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar_grid.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN 
+	#astar_grid.jumping_enabled = true
 	astar_grid.update()
 	
 	for x in tile_map.get_used_rect().size.x:
 		for y in tile_map.get_used_rect().size.y:
 			var tile_position = Vector2i(
-				x + tile_map.get_used_rect().position.x,
-				y + tile_map.get_used_rect().position.y
+				x + int(astar_grid.region.position.x),
+				y + int(astar_grid.region.position.y)
 			)
 			
 			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
@@ -30,9 +34,7 @@ func _input(event):
 	if event.is_action_pressed("move") == false:
 		return
 	
-	var id_path = astar_grid.get_id_path(
-		tile_map.local_to_map(global_position),
-		tile_map.local_to_map(get_global_mouse_position()))
+	var id_path
 	
 	if is_moving:
 		id_path = astar_grid.get_id_path(
@@ -41,16 +43,17 @@ func _input(event):
 		)
 		print("is_moving: ", id_path)
 	else:
-		astar_grid.get_id_path(
+		id_path = astar_grid.get_id_path(
 			tile_map.local_to_map(global_position),
 			tile_map.local_to_map(get_local_mouse_position())
 		)
 		print("else: ", id_path)
+		
 	if id_path.is_empty() == false:
 		current_id_path = id_path
 	
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if current_id_path.is_empty():
 		return
 	
