9885a330d6c3c032346253e251504add8fd53450
Mathieu Grondin <mgrondin.mg@gmail.com>
2024-03-17 11:16:50 -0400

    Add mort screen

---
+++ jam_cobayes_2024/scenes/mort_label.gd
@@ -0,0 +1,35 @@
+extends VBoxContainer
+
+var titre :Dictionary = {
+	0: "L'orgueil a eu raison de toi",
+	1: "L'avarice a eu raison de toi",
+	2: "L'envie a eu raison de toi",
+	3: "La colère a eu raison de toi", 
+	4: "La luxure a eu raison de toi",
+	5: "La gourmandise a eu raison de toi", 
+	6: "La paresse a eu raison de toi"
+}
+
+var sousTitre :Dictionary = {
+	0: "Tu crois maintenant que tu as toujours raison. Lourd...",
+	1: "Tu ne seras éternellement insatisfait!",
+	2: "La jalousie maladive t'a emportée.",
+	3: "Tu es devenu bélier ascendant colérique!", 
+	4: "Ta richesse a semé la solitude autour de toi.",
+	5: "Tes sentiments ont fini par te manger...", 
+	6: "Te voilà fossilisé dans l'inaction."
+}
+
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var from = 0
+	var to = titre.size() - 1
+	var index = rng.randi_range(from, to)
+	$Title.text = titre[index]
+	$SubTitle.text = sousTitre[index]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
