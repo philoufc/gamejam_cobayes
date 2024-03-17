9c01578dc584d67d94eb459807c9b20dc9276d1b
Ludo <ludodescary@gmail.com>
2024-03-17 09:50:02 -0400

    - ajout animaux

---
+++ jam_cobayes_2024/scenes/pigeon.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+var is_moving: bool
+@onready var tile_map = $"../../World/TileMap"
+@onready var player = $"../../Player"
+@onready var astar_grid: AStarGrid2D
+@onready var target_position: Vector2
+@onready var tile_map_rect: Rect2
+const SPEED = 100
+
+func _ready():
+	astar_grid = player.astar_grid
+	$AnimatedSprite2D.play("pigeon_side")
+	tile_map_rect = tile_map.get_used_rect()
+	update_target_position()
+	print(target_position)
+
+func _physics_process(delta):
+	if global_position != target_position:
+		global_position = global_position.move_toward(target_position, delta * SPEED)
+
+
+func _on_timer_timeout():
+	update_target_position()
+
+func update_target_position():
+	var x = int(tile_map_rect.position.x) + (randi() % int(tile_map_rect.size.x))
+	var y = int(tile_map_rect.position.y) + (randi() % int(tile_map_rect.size.y))
+	target_position = tile_map.map_to_local(Vector2(x,y))
+	
